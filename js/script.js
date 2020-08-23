20c7df4741e1089e86eba0d4d4f1b8e61f5f47c6
KenanyaKAP <kenanyaline@gmail.com>
2020-08-23 22:46:48 +0700

    Change Navbar Behaviour

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
 	// Scroll Event
 	$('#scrollDiv').hide();
 	$(window).scroll(function() {
+
+		//Background fading
 		if ($(document).scrollTop() > $('.member').offset().top - 400) {
 			$('.navbar').css("background-color", "#141414");
 			$('.navbar-brand').css("font-size", "20px");
@@ -33,15 +35,24 @@ $(document).ready(function() {
 				$('.navbar').css("padding-top", "50px");
 			}
 		}
+
+		// Closing Navbar
+		$('.collapsedd').each(function(i, obj) {
+			if ($(obj).attr("aria-expanded") == 'true'){
+				$($(obj).attr('href')).collapse('hide');
+			}
+		});
 	});
 
 	// On Click Event
 	$(document).click(function () {
 
 		// Closing Navbar
-		if ($('.dropdown-toggle').attr("aria-expanded") == 'true'){
-			$($('.dropdown-toggle').attr('href')).collapse('hide');
-		}
+		$('.dropdown-toggle.collapsedd').each(function(i, obj) {
+			if ($(obj).attr("aria-expanded") == 'true'){
+				$($(obj).attr('href')).collapse('hide');
+			}
+		});
 
 		// alert($('.member h1').css('font-size'));
 	});
