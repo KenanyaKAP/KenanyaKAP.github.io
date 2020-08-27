1fe370bdbfc7c226fcfd3b181b04ca96c280dd4d
KenanyaKAP <kenanyaline@gmail.com>
2020-08-27 11:15:36 +0700

    Adding PKWU Materials

--- PKWUMaterials/script.js
+++ PKWUMaterials/script.js
@@ -5,7 +5,7 @@ $('.page-scroll').on('click', function(){
 
 // Nav Button
 $('.navbar-toggler').on('click', function(){
-	if ($(document).scrollTop() <= $('.content').offset().top - 400) {
+	if ($(document).scrollTop() <= $('.jumbotrons').offset().top + 50) {
 		if ($('.navbar-toggler').attr("aria-expanded") == 'false'){
 			$('.navbar').css("background-color", "#141414");
 		} else {
@@ -20,25 +20,6 @@ $(document).ready(function() {
 	// Scroll Event
 	$('#scrollDiv').hide();
 	$(window).scroll(function() {
-
-		//Navbar Background fading
-		if ($(document).scrollTop() > $('.content').offset().top - 400) {
-			$('.navbar, .dropdown-menu').css("background-color", "#141414");
-			$('.navbar-brand').css("font-size", "20px");
-			if ($('html,body').width() > 974){
-				$('.navbar').css("padding-top", "10px");
-			}
-		} else {
-			$('.navbar').css("background-color", "#14141400");
-			$('.navbar-brand').css("font-size", "30px");
-			if ($('html,body').width() > 974){
-				$('.navbar').css("padding-top", "50px");
-				$('.dropdown-menu').css("background-color", "rgba(0, 0, 0, 0.5)");
-			} else {
-				$('.dropdown-menu').css("background-color", "transparent");
-			}
-		}
-
 		// Closing Navbar
 		$('.collapsedd').each(function(i, obj) {
 			if ($(obj).attr("aria-expanded") == 'true'){
@@ -57,6 +38,6 @@ $(document).ready(function() {
 			}
 		});
 
-		// alert($('.nav-item a').css('padding'));
+		// alert($('.navbar').css('height'));
 	});
 });
