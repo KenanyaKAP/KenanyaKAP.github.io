08368c81bdb2157026880db4e3266310c826eacd
KenanyaKAP <69131654+KenanyaKAP@users.noreply.github.com>
2022-05-18 13:45:41 +0700

    Create home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./public/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto Mono'],
+        'catamaran': ['Catamaran'],
+      },
+    },
   },
   plugins: [],
 }
