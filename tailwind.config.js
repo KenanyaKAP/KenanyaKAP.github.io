92516c8771018154966b7d675d0bcb7bf372dfdd
KenanyaKAP <69131654+KenanyaKAP@users.noreply.github.com>
2022-05-21 15:03:33 +0700

    Add Project Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  content: ['./*.{html, js}'],
+  content: ['./*.html', './js/*.js'],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         'roboto': ['Roboto Mono'],
         'catamaran': ['Catamaran'],
+        'odibee': ['Odibee Sans'],
       },
       screens: {
         '2xl': '1320px',
