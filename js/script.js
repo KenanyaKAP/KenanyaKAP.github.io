9d30c2de91d98763896b1b8cf09d29ddf965a489
KenanyaKAP <69131654+KenanyaKAP@users.noreply.github.com>
2022-05-22 23:19:22 +0700

    Add Project section

--- js/script.js
+++ js/script.js
@@ -23,3 +23,22 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   darkToggle.checked = false;
   circle.innerHTML = '<svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 mr-2"><path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke="#38bdf8"></path><path d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836" stroke="#38bdf8"></path></svg>'
 }
+
+// Slide
+let slideIndex = 0;
+const slides = document.getElementsByClassName('slide-container');
+slideFunc();
+function slideFunc() {
+  for (let i = 0; i < slides.length; i++) {
+    let imgs = slides[i].getElementsByClassName('slide-img');
+    for (let j = 0; j < imgs.length; j++) {
+      imgs[j].classList.add('invisible');
+    }
+    imgs[slideIndex % imgs.length].classList.remove('invisible');
+    imgs[slideIndex % imgs.length].classList.remove('fade-inout');
+    imgs[slideIndex % imgs.length].offsetWidth;
+    imgs[slideIndex % imgs.length].classList.add('fade-inout');
+  }
+  slideIndex++;
+  setTimeout(slideFunc, 5000);
+}
